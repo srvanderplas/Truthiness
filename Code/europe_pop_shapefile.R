1bc7dd0b4ef2035d8606ee517f1de7dea91fc2eb
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-07-24 16:36:49 -0500

    add vignette link

--- Code/europe_pop_shapefile.R
+++ Code/europe_pop_shapefile.R
@@ -5,3 +5,5 @@ euro_sf <- st_read("Data/Version 2_0_1/GEOSTATReferenceGrid/Grid_ETRS89_LAEA_1K-
 euro_pop <- read_csv("Data/Version 2_0_1/JRC-GHSL_AIT-grid-POP_1K_2011.csv")
 
 euro_sf <- left_join(euro_sf, euro_pop)
+
+# https://cran.r-project.org/web/packages/sf/vignettes/sf2.html
