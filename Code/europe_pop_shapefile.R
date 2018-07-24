bf91f95f23f7bac58d0fd6d99c2549fe3170262f
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-07-24 12:54:43 -0500

    Belgium facts - incomplete

---
+++ Code/europe_pop_shapefile.R
@@ -0,0 +1,7 @@
+library(sf)
+
+euro_sf <- st_read("Data/Version 2_0_1/GEOSTATReferenceGrid/Grid_ETRS89_LAEA_1K-ref_GEOSTAT_POP_2011_V2_0_1.shp")
+
+euro_pop <- read_csv("Data/Version 2_0_1/JRC-GHSL_AIT-grid-POP_1K_2011.csv")
+
+euro_sf <- left_join(euro_sf, euro_pop)
