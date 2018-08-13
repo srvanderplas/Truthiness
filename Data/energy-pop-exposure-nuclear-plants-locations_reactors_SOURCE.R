bdfc29f3f7c8bba9f1df27c7b5b42e4d7a62d74f
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-08-13 09:21:02 -0500

    Source of nuclear plant location data

---
+++ Data/energy-pop-exposure-nuclear-plants-locations_reactors_SOURCE.R
@@ -0,0 +1,2 @@
+# Source of nuclear plant location data: 
+http://sedac.ciesin.columbia.edu/downloads/data/energy/energy-pop-exposure-nuclear-plants-locations/energy-pop-exposure-nuclear-plants-locations.zip
