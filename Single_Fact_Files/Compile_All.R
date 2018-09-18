9bde705592294974a05a8a691106ae7a25b77bf0
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-09-18 11:42:29 -0500

    multicore compilation

--- Single_Fact_Files/Compile_All.R
+++ Single_Fact_Files/Compile_All.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(rmarkdown)
+library(furrr)
 
 rmdfiles <- list.files(here::here("Single_Fact_Files"), ".Rmd", recursive = T, full.names = T)
 
-purrr::walk(rmdfiles, function(x) {
+plan(multiprocess, workers = 12)
+furrr::future_map(rmdfiles, function(x) {
   rmarkdown::render(x, output_format = "pdf_document", output_dir = here::here("Single_Fact_Files/Final"))
 })
