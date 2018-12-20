d0d92b5f3ba9c9260267fc3291fb6aaa10f79bc2
srvander <srvanderplas@gmail.com>
2018-12-20 09:12:08 -0600

    Add all files -> one PDF and compress

--- Facts/Single_Fact_Files/Compile_All.R
+++ Facts/Single_Fact_Files/Compile_All.R
@@ -16,3 +16,10 @@ re_do <- which(sapply(res, class) != "character")
 if (length(re_do) > 0) {
   res[re_do] <- furrr::future_map(rmdfiles[re_do], compile_doc)
 }
+
+# Create one giant pdf
+list.files(here::here("Facts/Single_Fact_Files/Final"), "*.pdf", full.names = T) %>%
+  staplr::staple_pdf(input_files = ., output_filepath = here::here("Facts/Single_Fact_Files/All_Facts.pdf"))
+
+# Run as system command
+system("ps2pdf /home/srvander/Projects/CSAFE/Truthiness/Facts/Single_Fact_Files/All_Facts.pdf /home/srvander/Projects/CSAFE/Truthiness/Facts/Single_Fact_Files/All_Facts_Small.pdf")
