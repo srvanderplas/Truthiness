bd1851caf2079eba7ceaecb3fa43deb6aeb0bcc3
srvander <srvanderplas@gmail.com>
2018-12-20 11:42:45 -0600

    processing code
    Former-commit-id: 899321302bda17a211a965a6f2f3125799a75751

--- Facts/Code/Rmd_to_R.R
+++ Facts/Code/Rmd_to_R.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 
 rmdfiles <- list.files(path = ".", pattern = ".Rmd", full.names = T, recursive = T)
-rmdfiles <- rmdfiles[!str_detect(rmdfiles, "Truthiness|Facts")]
+rmdfiles <- rmdfiles[str_detect(rmdfiles, "Single_Fact_Files")]
 
 purl_in_folder <- function(x) {
   dir <- dirname(x)
-  indicname <- str_replace_all(dir, "[\\./]", "")
-  Rfile <- file.path("Single_Fact_Code", str_replace(basename(x), ".Rmd", ".R"))
+  indicname <- str_replace_all(dir, "[\\./]", "") %>% str_remove_all("Facts|Single_Fact_Files")
+  Rfile <- file.path(here::here("Facts/Single_Fact_Code"), str_replace(basename(x), ".Rmd", ".R"))
   knitr::purl(x, output = Rfile)
-  imgs <- list.files(dir, pattern = "png$|jpg$", full.names = T)
-  imgnewnames <- file.path("Pictures_all", paste0(indicname, "-", basename(imgs)))
-  file.copy(imgs, imgnewnames)
+  # imgs <- list.files(dir, pattern = "png$|jpg$", full.names = T)
+  # imgnewnames <- file.path(here::here("Facts/Pictures_all"), paste0(indicname, "-", basename(imgs)))
+  # file.copy(imgs, imgnewnames, overwrite = T)
 }
 
 lapply(rmdfiles, purl_in_folder)
 
 setwd("Single_Fact_Code")
 # find ./ -type f -exec sed -i 's!\(\"\.\./\)\(.*\)\(\"\)!here::here("\2")!g' {} \;
-system("find ./ -type f -exec sed -i 's!\\(\\\"\\.\\./\\)\\(.*\\)\\(\\\"\\)!here::here(\\\"\\2\\\")!g' {} \\;")
+# system("find ./ -type f -exec sed -i 's!\\(\\\"\\.\\./\\)\\(.*\\)\\(\\\"\\)!here::here(\\\"\\2\\\")!g' {} \\;")
